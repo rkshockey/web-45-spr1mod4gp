c17b2d3c2f4a2c445ebd9792c9e4d3a8b692dcfa
rkshockey <rkshockey@gmail.com>
2021-09-02 13:08:26 -0400

    ready to deploy

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-//eslint-disable-next-line
-const server = require('express')()
+require('dotenv').config() // allows to stash artificial env variables
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
 
-server.listen(5000, () => {
-    console.log(`Server is listening on port 5000`)
+const server = express()
+
+server.use(express.json())
+server.use(cors())
+server.use(morgan('dev'))
+
+server.get('/', (req, res) => {
+    res.send('<h1>Web45 Base Server</h1>')
+})
+
+server.get('/api', (req, res) => {
+    res.json({message: 'Web45 rocks!'})
+})
+
+const port = process.env.PORT || 5000
+
+server.listen(port, () => {
+    console.log(`Server is listening on port ${port}`)
 })
